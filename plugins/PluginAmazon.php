e92eb1f2056cf1353964df451c912eda1262be21
Bryan Selner <bryanselner@hotmail.com>
2014-07-04 11:47:03 -0700

    Fixes for Amazon Plugin
    Fixed:  Amazon plugin fails to return results and terminates the
    job_scooper run
    Added <table> around HTML download and re-wrote job posting parser to
    address issue
    Added commented out test code in Applescript that returns just 2 pages
    so that while debugging you don't have to wait for 10+ pages to return
    Started to work on returning team name and team category from job
    posting (code currently commented out because of bug with
    strip_punctuation)
    Removed previous debugging log line of Amazon Job ID.

--- plugins/PluginAmazon.php
+++ plugins/PluginAmazon.php
@@ -31,7 +31,55 @@ class PluginAmazon extends ClassJobsSitePlugin
     protected $strFilePath_HTMLFileDownloadScript = "PluginAmazon_downloadjobs.applescript";
 
     function parseJobsListForPage($objSimpHTML)
+    {
+        $ret = array();
+        $nodesTR= $objSimpHTML->find('tr');
 
+        $nTRIndex = 0;
+        while($nTRIndex < count($nodesTR))
+        {
+            $item = $this->getEmptyJobListingRecord();
+
+            $tds = $nodesTR[$nTRIndex]->find('td');
+
+            if(isset($tds) && count($tds) > 0)
+            {
+
+                $titleLink = $tds[1]->find("a");
+
+                $item['job_title'] = $titleLink[0]->plaintext;
+                $item['job_post_url'] = $titleLink[0]->href;
+                $item['company'] = 'Amazon';
+
+                $item['job_site'] = 'Amazon';
+                $item['date_pulled'] = \Scooper\getTodayAsString();
+
+                $item['job_id'] = trim(explode("/", $item['job_post_url'])[4]);
+
+/*
+BUGBUG:  strip_punctuation in strScrub returns null for some reason.
+
+                $teamDetails = combineTextAllChildren($tds[4]);
+                $strTeamCat = preg_replace("/\bTeam:\s{2,}(.*)\s{2,}Team Category:\s{2,}(.*)\s{2,}Short Description:\s{2,}(.*)/", "$1; $2", $teamDetails );
+                $strTeamCat = \Scooper\strScrub($strTeamCat, ADVANCED_TEXT_CLEANUP);
+
+                $item['job_site_category'] =  $strTeamCat . "; ". $tds[2]->plaintext ;
+ */
+                $item['job_site_category'] =  $tds[2]->plaintext ;
+
+                $item['location'] = $tds[3]->plaintext ;
+
+
+                $ret[] = $this->normalizeItem($item);
+
+            }
+
+            $nTRIndex = $nTRIndex + 1;
+
+        }
+        return $ret;
+    }
+    function parseJobsListForPageOrig($objSimpHTML)
     {
         $ret = array();
         $nodesTD= $objSimpHTML->find('tr td[class="expand footable-first-column"]');
@@ -42,6 +90,8 @@ class PluginAmazon extends ClassJobsSitePlugin
             if($nodesTD[$nTDIndex])
             {
                 $item = $this->getEmptyJobListingRecord();
+
+                \SimpleHtmlDom\dump_html_tree($nodesTD[$nTDIndex]);
 
                 $titleObj = $nodesTD[$nTDIndex]->nextSibling();
 
@@ -54,6 +104,9 @@ class PluginAmazon extends ClassJobsSitePlugin
                 $item['date_pulled'] = \Scooper\getTodayAsString();
 
                 $item['job_id'] = trim(explode("/", $item['job_post_url'])[4]);
+
+                $GLOBALS['logger']->logLine("AZID=".$item['job_id'], \Scooper\C__DISPLAY_ITEM_DETAIL__);
+
 
                 $catObj = $titleObj->nextSibling();
                 $item['job_site_category'] = $catObj->plaintext;
