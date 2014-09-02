995da23fe363c2d7f79850c8697e3ea5e259a5f9
Bryan Selner <bryanselner@hotmail.com>
2014-09-01 17:58:31 -0700

    Added global INI setting for disabling a class of site plugins
    Useful when running on different platforms and need to disable
    applescript plugins.
    
    [global_search_options]
    excluded_plugin_types[]="Applescript"

--- include/ClassConfig.php
+++ include/ClassConfig.php
@@ -24,7 +24,7 @@ class ClassConfig extends ClassJobsSitePlugin
     protected $nNumDaysToSearch = -1;
     protected $arrFileDetails = array('output' => null, 'output_subfolder' => null, 'config_ini' => null, 'user_input_files' => null);
     protected $arrEmailAddresses = null;
-    protected $configSettings = array('searches' => null, 'keyword_sets' => null, 'location_sets' => null, 'number_days'=>VALUE_NOT_SUPPORTED, 'included_sites' => array(), 'excluded_sites' => array());
+    protected $configSettings = array('searches' => null, 'keyword_sets' => null, 'location_sets' => null, 'number_days'=>VALUE_NOT_SUPPORTED, 'included_sites' => array(), 'excluded_sites' => array(), 'excluded_plugin_types' => array());
     protected $arrEmail_PHPMailer_SMTPSetup = null;
 
 
@@ -462,6 +462,41 @@ class ClassConfig extends ClassJobsSitePlugin
                     $excludedPlugins = strtolower($excludedPlugins);
                     $this->configSettings['excluded_plugin_types'][$excludedPlugins] = $excludedPlugins;
                 }
+
+                if(isset($this->configSettings['excluded_plugin_types']) && count($this->configSettings['excluded_plugin_types']) > 0)
+                {
+                    foreach($this->configSettings['excluded_plugin_types'] as $type)
+                    {
+                        $flagCheck=null;
+                        foreach($GLOBALS['DATA']['site_plugins'] as $plugin)
+                        {
+                            $class = new $plugin['class_name'];
+                            switch ($type)
+                            {
+                                case "applescript":
+                                    $flagCheck = C__JOB_SEARCH_RESULTS_TYPE_HTML_FILE__;
+                                    break;
+
+                                case "web":
+                                    $flagCheck = C__JOB_SEARCH_RESULTS_TYPE_WEBPAGE__;
+                                    break;
+
+                                case "xml":
+                                    $flagCheck = C__JOB_SEARCH_RESULTS_TYPE_XML__;
+                                    break;
+                            }
+
+                            if(isset($flagCheck) && $class->isBitFlagSet($flagCheck))
+                            {
+                                $this->configSettings['excluded_sites'][$plugin['name']] = $plugin['name'];
+                                if(isset($this->configSettings['included_sites'][$plugin['name']])) unset($this->configSettings['included_sites'][$plugin['name']]);
+
+                            }
+
+                        }
+                    }
+                }
+
             }
         }
     }
