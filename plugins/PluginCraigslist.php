f9ba98425bdb9fc65862d280041fa4ee7ee8d440
Bryan Selner <dev@bryanselner.com>
2017-01-08 19:07:45 -0800

    Plugins/Craigslist: * fixed duplicate tag_link value * fixed no index for most tags * BUGBUG: craigslist treats sub-rows differently for counting results in different cases.  When a single page of results is returned, they are included in the overall count.  But when a multi-page result set is returned, they are not! Setting C__JOB_IGNORE_MISMATCHED_JOB_COUNTS to work around this.

--- plugins/PluginCraigslist.php
+++ plugins/PluginCraigslist.php
@@ -26,21 +26,22 @@ class PluginCraigslist extends ClassClientHTMLJobSitePlugin
     protected $siteBaseURL = 'http://seattle.craigslist.org';
     protected $strBaseURLFormat = "http://***LOCATION***.craigslist.org/search/jjj?bundleDuplicates=1&query=%22***KEYWORDS***%22&srchType=T&searchNearby=1&s=***ITEM_NUMBER***";
 //    protected $strBaseURLFormat = "http://***LOCATION***.craigslist.org/search/jjj?s=***ITEM_NUMBER***&catAbb=jjj&query=***KEYWORDS***&srchType=T&bundleDuplicates=1";
-    protected $additionalFlags = [C__JOB_LOCATION_REQUIRES_LOWERCASE, C__JOB_DAYS_VALUE_NOTAPPLICABLE__, C__JOB_KEYWORD_SUPPORTS_QUOTED_KEYWORDS, C__JOB_PAGE_VIA_URL];
+    // BUGBUG: craigslist treats sub-rows differently for counting results in different cases.  When a single page of results is returned, they are included in the overall count
+    //         But when a multi-page result set is returned, they are not! Setting C__JOB_IGNORE_MISMATCHED_JOB_COUNTS to work around this.
+    protected $additionalFlags = [C__JOB_LOCATION_REQUIRES_LOWERCASE, C__JOB_DAYS_VALUE_NOTAPPLICABLE__, C__JOB_KEYWORD_SUPPORTS_QUOTED_KEYWORDS, C__JOB_PAGE_VIA_URL, C__JOB_IGNORE_MISMATCHED_JOB_COUNTS];
     protected $typeLocationSearchNeeded = 'location-city';
     protected $strKeywordDelimiter = "|";
 
     protected $arrListingTagSetup = array(
         'tag_listings_noresults' => array('tag' => 'div', 'attribute'=>'class', 'attribute_value' => 'noresults', 'return_attribute' => 'plaintext', 'return_value_callback' => "PluginCraigslist::getNoResultsCount"),
-        'tag_listings_count' => array('tag' => 'span', 'attribute'=>'class', 'attribute_value' => 'totalcount'  , 'index'=> 0, 'return_attribute' => 'plaintext'),
+        'tag_listings_count' => array('tag' => 'span', 'attribute'=>'class', 'attribute_value' => 'totalcount', 'index'=> 0, 'return_attribute' => 'plaintext'),
         'tag_listings_section' => array('selector' => 'li[data-pid]', 'return_value_callback' => "PluginCraigslist::filterListingNodes"),
-        'tag_title' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'return_attribute' => 'plaintext'),
-        'tag_link' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'return_attribute' => 'href'),
-        'tag_job_id' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'return_attribute' => 'data-id', 'return_value_callback' => "PluginCraigslist::getNoResultsCount"),
-        'tag_link' => array('tag' => 'a', 'attribute' => 'class', 'attribute_value' => 'result-title hdrlnk', 'return_attribute' => 'href'),
-        'tag_department' => array('tag' => 'td', 'attribute' => 'class', 'attribute_value' =>'listing-department'),
-        'tag_location' => array('tag' => 'span', 'attribute' => 'class', 'attribute_value' =>'result-hood'),
-        'tag_postdate' => array('tag' => 'time', 'attribute' => 'class', 'attribute_value' =>'result-date')
+        'tag_link' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'index'=> 0, 'return_attribute' => 'href'),
+        'tag_title' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'index'=> 0, 'return_attribute' => 'plaintext'),
+        'tag_job_id' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'index'=> 0, 'return_attribute' => 'data-id', 'return_value_callback' => "PluginCraigslist::getNoResultsCount"),
+        'tag_department' => array('tag' => 'td', 'attribute' => 'class', 'attribute_value' =>'listing-department', 'index'=> 0),
+        'tag_location' => array('tag' => 'span', 'attribute' => 'class', 'attribute_value' =>'result-hood', 'index'=> 0),
+        'tag_postdate' => array('tag' => 'time', 'attribute' => 'class', 'attribute_value' =>'result-date', 'index'=> 0)
     );
 
     function getNoResultsCount($var)
