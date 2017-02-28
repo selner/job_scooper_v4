ccaa406b2cda4264e06843ff9b5cf7b629a6e152
Bryan Selner <dev@bryanselner.com>
2017-02-27 20:34:45 -0800

    Plugins/Talentbrew: * Fixed Disney plugin not returning results due to a job site HTML change * Fixed results count bug in new HP plugin caused by earlier changes

--- src/plugins/ats_platforms/talentbrew.php
+++ src/plugins/ats_platforms/talentbrew.php
@@ -47,7 +47,6 @@ class PluginBoeing extends BasePluginTalentBrew
 
     function __construct($strBaseDir)
     {
-        $this->arrListingTagSetup['tag_pages_count'] = array(array('tag' => 'span', 'attribute' => 'class', 'attribute_value' => 'pagination-total-pages'), 'return_attribute' => 'plaintext', 'return_value_regex' =>  '/of (.*)/');
         parent::__construct($strBaseDir);
     }
 }
@@ -57,12 +56,15 @@ class PluginDisney extends BasePluginTalentBrew
     protected $siteName = 'Disney';
     protected $siteBaseURL = 'https://jobs.disneycareers.com';
 //    protected $additionalFlags = [ C__JOB_CLIENTSIDE_INFSCROLLPAGE ];
+    protected $additionalFlags = [ C__JOB_ITEMCOUNT_NOTAPPLICABLE__ ];
     protected $nJobListingsPerPage = 15;
 
     function __construct($strBaseDir = null)
     {
-
         parent::__construct($strBaseDir);
+        unset($this->arrListingTagSetup['tag_listings_count']);  // hp doesn't support a results count
+        $this->arrListingTagSetup['tag_listings_section'] = array(array('tag' => 'section', 'attribute' => 'id', 'attribute_value' => 'search-results-list'), array('tag' => 'table'),array('tag' => 'tr'));
+
         $this->strBaseURLFormat = $this->strBaseURLFormat  . "?orgIds=391-5733-5732&kt=1";
     }
 
@@ -82,9 +84,8 @@ class BasePluginTalentBrew extends ClassClientHTMLJobSitePlugin
     protected $nJobListingsPerPage = 50;
 
     protected $arrListingTagSetup = array(
-        'tag_listings_count' => array(array('tag' => 'section', 'attribute' => 'id', 'attribute_value' => 'search-results'), array('tag' => 'h1'), 'return_attribute' => 'plaintext', 'return_value_regex' =>  '/(.*?) [Rr]esults.*/'),
-//        'tag_listings_count' => array(array('tag' => 'section', 'attribute' => 'id', 'attribute_value' => 'search-results'), 'return_attribute' => 'data-total-results'),
-
+        'tag_listings_count' => array(array('tag' => 'section', 'attribute' => 'id', 'attribute_value' => 'search-results'), array('tag' => 'h1'), 'return_attribute' => 'plaintext', 'return_value_regex' =>  '/(.*?) .*/'),
+        'tag_pages_count' => array(array('tag' => 'span', 'attribute' => 'class', 'attribute_value' => 'pagination-total-pages'), 'return_attribute' => 'plaintext', 'return_value_regex' =>  '/of (.*)/'),
         'tag_listings_section' => array(array('tag' => 'section', 'attribute' => 'id', 'attribute_value' => 'search-results-list'), array('tag' => 'ul'),array('tag' => 'li')),
         'tag_title' =>  array(array('tag' => 'a'), array('tag' => 'h2')),
         'tag_link' =>  array(array('tag' => 'a'), 'return_attribute' => 'href'),
