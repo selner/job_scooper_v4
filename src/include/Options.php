220cf8d14079e1cfea171a99d82eb81dfced078e
Bryan Selner <dev@bryanselner.com>
2017-03-05 12:25:05 -0800

    Updated app version to v4.alpha1

--- src/include/Options.php
+++ src/include/Options.php
@@ -17,7 +17,7 @@
 //
 // If installed as part of the package, uses Klogger v0.1 version (http://codefury.net/projects/klogger/)
 //
-define('__APP_VERSION__', "Job Scooper v4");
+define('__APP_VERSION__', "Job Scooper v4.alpha1");
 
 if (!strlen(__ROOT__) > 0) { define('__ROOT__', dirname(dirname(__FILE__))); }
 require_once(__ROOT__.'/lib/pharse.php');
@@ -158,7 +158,7 @@ function is_OptionIncludedSite($strName)
             $GLOBALS['OPTS'][$strIncludeSiteKey] = true;
             $ret = $GLOBALS['OPTS'][$strIncludeSiteKey];
         }
-
+    
 
 
     return $ret;
