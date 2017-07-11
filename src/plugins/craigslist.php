46875f319674a4095f79dce4f79765e8e5dd4077
Bryan Selner <dev@bryanselner.com>
2017-07-11 12:12:24 -0700

    Fixes for Craiglist plugin: * Removed duplicate bundling.  That URL value was causing it to be near impossible to get an accurate set of listings returned.  Also let me simplify it to not have to use a callback to filter out dupes. * Removed the IGNORE_MISMATCHED_JOBCOUNT setting.  We now get the right results. * Updated tagging to use selectors where possible * Fixed no results detection

--- src/plugins/craigslist.php
+++ src/plugins/craigslist.php
@@ -22,49 +22,33 @@ require_once(__ROOT__.'/include/ClassJobsSiteCommon.php');
 class PluginCraigslist extends ClassClientHTMLJobSitePlugin
 {
     protected $siteName = 'Craigslist';
-    protected $nJobListingsPerPage = 100;
+    protected $nJobListingsPerPage = 120;
     protected $siteBaseURL = 'http://seattle.craigslist.org';
-    protected $strBaseURLFormat = "http://***LOCATION***.craigslist.org/search/jjj?bundleDuplicates=1&query=%22***KEYWORDS***%22&srchType=T&searchNearby=1&s=***ITEM_NUMBER***";
+    protected $strBaseURLFormat = "http://***LOCATION***.craigslist.org/search/jjj?query=***KEYWORDS***&srchType=T&searchNearby=1&s=***ITEM_NUMBER***";
 //    protected $strBaseURLFormat = "http://***LOCATION***.craigslist.org/search/jjj?s=***ITEM_NUMBER***&catAbb=jjj&query=***KEYWORDS***&srchType=T&bundleDuplicates=1";
     // BUGBUG: craigslist treats sub-rows differently for counting results in different cases.  When a single page of results is returned, they are included in the overall count
     //         But when a multi-page result set is returned, they are not! Setting C__JOB_IGNORE_MISMATCHED_JOB_COUNTS to work around this.
-    protected $additionalFlags = [C__JOB_LOCATION_REQUIRES_LOWERCASE, C__JOB_KEYWORD_SUPPORTS_QUOTED_KEYWORDS, C__JOB_IGNORE_MISMATCHED_JOB_COUNTS];
+    protected $additionalFlags = [C__JOB_LOCATION_REQUIRES_LOWERCASE, C__JOB_KEYWORD_SUPPORTS_QUOTED_KEYWORDS];
     protected $typeLocationSearchNeeded = 'location-city';
-    protected $paginationType = C__PAGINATION_PAGE_VIA_URL;
     protected $strKeywordDelimiter = "|";
+    protected $paginationType = C__PAGINATION_PAGE_VIA_URL;
 
     protected $arrListingTagSetup = array(
-        'tag_listings_noresults' => array('tag' => 'div', 'attribute'=>'class', 'attribute_value' => 'noresults', 'return_attribute' => 'plaintext', 'return_value_callback' => "isNoJobResults"),
-        'tag_listings_count' => array('tag' => 'span', 'attribute'=>'class', 'attribute_value' => 'totalcount', 'index'=> 0, 'return_attribute' => 'plaintext'),
-        'tag_listings_section' => array('selector' => 'li[data-pid]', 'return_value_callback' => "filterListingNodes"),
+        'tag_listings_noresults' => array('selector' => 'div.noresults', 'return_attribute' => 'plaintext', 'return_value_callback' => "isNoJobResults"),
+        'tag_listings_count' => array('selector' => 'span.totalcount', 'index'=> 0, 'return_attribute' => 'plaintext'),
+        'tag_listings_section' => array('selector' => '#sortable-results ul li'),
         'tag_link' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'index'=> 0, 'return_attribute' => 'href'),
         'tag_title' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'index'=> 0, 'return_attribute' => 'plaintext'),
         'tag_job_id' => array('tag' => '*', 'attribute' => 'class', 'attribute_value' => 'hdrlnk', 'index'=> 0, 'return_attribute' => 'data-id'),
-        'tag_department' => array('tag' => 'td', 'attribute' => 'class', 'attribute_value' =>'listing-department', 'index'=> 0),
-        'tag_location' => array('tag' => 'span', 'attribute' => 'class', 'attribute_value' =>'result-hood', 'index'=> 0),
-        'tag_job_posting_date' => array('tag' => 'time', 'attribute' => 'class', 'attribute_value' =>'result-date', 'index'=> 0)
+        'tag_department' => array('selector' => 'td.listing-department', 'index'=> 0),
+        'tag_location' => array('selector' => 'span.result-hood', 'index'=> 0),
+        'tag_job_posting_date' => array('selector' => 'time.result-date', 'index'=> 0)
     );
 
     static function isNoJobResults($var)
     {
-        return noJobStringMatch($var, "nothing found");
+        return noJobStringMatch($var, "Nothing found");
     }
-
-    static function filterListingNodes($var)
-    {
-        $retNodes = array();
-        foreach($var as $v)
-        {
-            $parent = $v->parentNode();
-            if(stristr($parent->class, "duplicate") != "")
-                continue;
-
-            $retNodes[] = $v;
-        }
-
-        return $retNodes;
-    }
-
 
     function getItemURLValue($nItem)
     {
