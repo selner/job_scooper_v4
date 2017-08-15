9414834ac76fc7038ac82e1a0b2bc23ad2c274bf
Bryan Selner <dev@bryanselner.com>
2017-08-15 12:45:56 -0700

    Added backup of custom code in generated-classes directory

--- build_data_model.sh
+++ build_data_model.sh
@@ -5,7 +5,10 @@ CONFIGDIR=`pwd`"/config"
 NOW=$(date "+%F-%H-%M-%S")
 
 cd $CONFIGDIR
-mv "jobscooper_db.sq3" "jobscooper_db.sq3-"$NOW
+mv "jobscooper_db.sq3" "jobscooper_db.sq3.backup-"$NOW
+rm -Rf ./generated-classes/Base
+rm -Rf ./generated-classes/Map
+cp -R ./generated-classes "./generated-classes.backup-"$NOW
 
 $PROPEL config:convert -vvv
 $PROPEL build -vvv
