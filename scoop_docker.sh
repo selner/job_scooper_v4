9df594f4a2a90c0ace7067cc49bbbfa3e8e19032
Bryan Selner <dev@bryanselner.com>
2017-09-16 11:42:39 -0700

    Testing path change

--- scoop_docker.sh
+++ scoop_docker.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-cd /opt/jobs_scooper/src
-
-
+cd /opt/jobs_scooper
 
 php runJobs.php -ini /var/local/jobs_scooper/configs/evan/job_scooper_config.ini -all -days 5 --output /var/local/jobs_scooper/output -notify=1
 
