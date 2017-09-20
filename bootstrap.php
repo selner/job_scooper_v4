707aadc4daf92c88c9f2fcdca009553ac3c8f026
Bryan Selner <selner@users.noreply.github.com>
2017-09-20 01:44:53 -0700

    Updated version to 1.0

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-define('__APP_VERSION__', "Job Scooper v4.master.beta-1");
+define('__APP_VERSION__', "Job Scooper v4.master.1.0");
 
 //use Doctrine\ORM\Tools\Setup;
 if (file_exists(dirname(__FILE__).'/vendor/autoload.php')) {
