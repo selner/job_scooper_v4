752f3e58ab7967dc8ede00ffa12fcde640051d79
Bryan Selner <dev@recoilvelocity.com>
2017-11-25 18:13:03 -0800

    Added new URL helper method "glue_url" to match PHP's parse_url

--- src/helpers/Misc.php
+++ src/helpers/Misc.php
@@ -747,3 +747,23 @@ function doExec($cmd)
     return $cmdOutput;
 }
 
+function glue_url($parsed) {
+    if (!is_array($parsed)) {
+        return false;
+    }
+
+    $uri = isset($parsed['scheme']) ? $parsed['scheme'].':'.((strtolower($parsed['scheme']) == 'mailto') ? '' : '//') : '';
+    $uri .= isset($parsed['user']) ? $parsed['user'].(isset($parsed['pass']) ? ':'.$parsed['pass'] : '').'@' : '';
+    $uri .= isset($parsed['host']) ? $parsed['host'] : '';
+    $uri .= isset($parsed['port']) ? ':'.$parsed['port'] : '';
+
+    if (isset($parsed['path'])) {
+        $uri .= (substr($parsed['path'], 0, 1) == '/') ?
+            $parsed['path'] : ((!empty($uri) ? '/' : '' ) . $parsed['path']);
+    }
+
+    $uri .= isset($parsed['query']) ? '?'.$parsed['query'] : '';
+    $uri .= isset($parsed['fragment']) ? '#'.$parsed['fragment'] : '';
+
+    return $uri;
+}
