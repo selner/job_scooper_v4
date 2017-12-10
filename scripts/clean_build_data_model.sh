7bc6d7a178f78127bcb02a599d57c4cfde60ef89
Bryan Selner <dev@recoilvelocity.com>
2017-12-09 17:33:58 -0800

    Added support for passing a different configuration file directory

--- scripts/clean_build_data_model.sh
+++ scripts/clean_build_data_model.sh
@@ -6,17 +6,26 @@ echo "*****************************************************************
 
 *****************************************************************"
 
+args=$#           # Number of args passed.
+ORIGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJDIR=`pwd`
+
+lastarg=${!args}
+if [ -z ${1} ]; then
+    CONFIGDIR="$PROJDIR/Config"
+else
+    CONFIGDIR=$1
+fi
 
 SCRIPTSDIR=`pwd`
 cd ..
 PROJDIR=`pwd`
 CODEDIR="$PROJDIR/src"
 PROPEL="$PROJDIR/vendor/bin/propel"
-CONFIGDIR="$PROJDIR/Config"
-OUTDIR=`echo ${JOBSCOOPER_OUTPUT}`
+OUTDIR=`echo $JOBSCOOPER_OUTPUT`
 NOW=$(date "+%F-%H-%M-%S")
 echo "current dir is `pwd`"
-
+
 echo "Moving current db to backup ($OUTDIR/job_scooper_db.sq3)..."
 mv "$OUTDIR/job_scooper_db.sq3" "$OUTDIR/job_scooper_db.sq3.backup-"$NOW
 
@@ -33,16 +42,20 @@ echo "*****************************************************************
 
 *****************************************************************"
 
-$PROPEL config:convert -vvv
-
-echo "Building Propel files starting from $PROJDIR"
-CMD="$PROPEL build -vvv"
+echo "Building Propel config file starting from $PROJDIR"
+CMD="$PROPEL config:convert -vvv --config-dir=$CONFIGDIR"
 echo $CMD; $CMD
 
-CMD="$PROPEL sql:build --overwrite -vvv"
+echo "Building Propel model files..."
+CMD="$PROPEL build -vvv --config-dir=$CONFIGDIR"
 echo $CMD; $CMD
 
-CMD="$PROPEL sql:insert -vvv"
+echo "Building Propel SQL files..."
+CMD="$PROPEL sql:build --overwrite -vvv --config-dir=$CONFIGDIR"
+echo $CMD; $CMD
+
+echo "Inserting SQL tables into database..."
+CMD="$PROPEL sql:insert -vvv --config-dir=$CONFIGDIR"
 echo $CMD; $CMD
 
 cp -f "$OUTDIR/job_scooper_db.sq3" "$CURDIR/examples/job_scooper_db.sq3"
