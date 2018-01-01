b606f45568f00aa08020659ee8bfd08eb0b8e59e
Bryan Selner <dev@recoilvelocity.com>
2017-12-31 22:16:49 -0800

    missed a rename spot

--- src/JobScooper/StageProcessor/JobsAutoMarker.php
+++ src/JobScooper/StageProcessor/JobsAutoMarker.php
@@ -80,7 +80,7 @@ class JobsAutoMarker
 
             $this->_markJobsList_SetOutOfArea_($arrJobs_AutoUpdatable);
 
-            $this->_markJobsList_UserExcludedKwdsViaQuery_($arrJobs_AutoUpdatable);
+            $this->_markJobsList_UserExcludedKeywords_($arrJobs_AutoUpdatable);
 
             $this->_markJobsList_SetAutoExcludedCompaniesFromRegex_($arrJobs_AutoUpdatable);
 
