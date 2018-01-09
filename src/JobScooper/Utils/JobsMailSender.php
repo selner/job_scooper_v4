86f7a4bedebf549ec5a87404db71c36dce555d42
Bryan Selner <dev@recoilvelocity.com>
2018-01-09 09:24:01 -0800

    JobsMailSender:   added more exception handling and error logging code to track down why one config is not sending mail successfully

--- src/JobScooper/Utils/JobsMailSender.php
+++ src/JobScooper/Utils/JobsMailSender.php
@@ -25,136 +25,182 @@ class JobsMailSender extends PHPMailer
 {
     private $alwaysNotify = false;
 
-    function __construct($alwaysNotify = false)
+	/**
+	 * JobsMailSender constructor.
+	 *
+	 * @param bool $alwaysNotify
+	 */
+	function __construct($alwaysNotify = false)
     {
         $this->alwaysNotify = $alwaysNotify;
         parent::__construct();
     }
 
-    function __destruct()
+	/**
+	 *
+	 */
+	function __destruct()
     {
 
     }
 
 
-    function sendEmail($strBodyText = null, $strBodyHTML = null, $arrDetailsAttachFiles = array(), $subject="No subject", $emailKind='results')
+	/**
+	 * @param null   $strBodyText
+	 * @param null   $strBodyHTML
+	 * @param array  $arrDetailsAttachFiles
+	 * @param string $subject
+	 * @param string $emailKind
+	 *
+	 * @return bool
+	 * @throws \Exception
+	 */
+	function sendEmail($strBodyText = null, $strBodyHTML = null, $arrDetailsAttachFiles = array(), $subject="No subject", $emailKind='results')
     {
-        if($this->alwaysNotify === false) {
-        	$skipNotify = getGlobalConfigOptionBoolean("command_line_args.disable-notifications");
-            if ($skipNotify === true) {
-                LogMessage(PHP_EOL . "User set -send_notifications = false so skipping email notification.)" . PHP_EOL);
-                LogMessage("Mail contents would have been:" . PHP_EOL . $strBodyText);
-                return null;
-            }
-        }
+    	try
+	    {
+
+	        if($this->alwaysNotify === false) {
+	            $skipNotify = getGlobalConfigOptionBoolean("command_line_args.disable-notifications");
+	            if ($skipNotify === true) {
+	                LogMessage(PHP_EOL . "User set -send_notifications = false so skipping email notification.)" . PHP_EOL);
+	                LogMessage("Mail contents would have been:" . PHP_EOL . $strBodyText);
+	                return null;
+	            }
+	        }
 
 
-        $smtpSettings = getConfigurationSetting('alerts.configuration.smtp');
+	        $smtpSettings = getConfigurationSetting('alerts.configuration.smtp');
 
-        if($smtpSettings != null && is_array($smtpSettings))
-        {
-            $this->isSMTP();
-            $properties = array_keys($smtpSettings);
-            foreach($properties as $property)
-            {
-	            $this->$property = $smtpSettings[$property];
-            }
+	        if($smtpSettings != null && is_array($smtpSettings))
+	        {
+	            $this->isSMTP();
+	            $properties = array_keys($smtpSettings);
+	            foreach($properties as $property)
+	            {
+		            $this->$property = $smtpSettings[$property];
+	            }
 
-        }
-        else
-        {
-	        $this->isSendmail();
-        }
+	        }
+	        else
+	        {
+		        $this->isSendmail();
+	        }
 
 
-        //
-        // Add initial email address header values
-        //
-	    $alerts_users = getConfigurationSetting("alerts." . $emailKind);
-        if(empty($alerts_users))
-        {
-        	//
-        	// hardcoded in the case where we were unable to load the email addresses for some reason
 	        //
-	        $this->addAnAddress("to", "dev@bryanselner.com", "JobScooper Deveopers");
-	        $this->setFrom("dev@bryanselner.com", "JobScooper Deveopers");
-	        $this->addReplyTo("dev@bryanselner.com", "JobScooper Deveopers" );
+	        // Add initial email address header values
+	        //
+		    $alerts_users = getConfigurationSetting("alerts." . $emailKind);
+	        if(empty($alerts_users))
+	        {
+	            //
+	            // hardcoded in the case where we were unable to load the email addresses for some reason
+		        //
+		        $this->addAnAddress("to", "dev@bryanselner.com", "JobScooper Deveopers");
+		        $this->setFrom("dev@bryanselner.com", "JobScooper Deveopers");
+		        $this->addReplyTo("dev@bryanselner.com", "JobScooper Deveopers" );
 
-        }
-        else {
-	        foreach ($alerts_users as $kind => $user) {
-		        switch ($kind) {
-			        case "from":
-				        $this->setFrom($user->getEmailAddress(), $user->getName());
+	        }
+	        else {
+		        foreach ($alerts_users as $kind => $user) {
+			        switch ($kind) {
+				        case "from":
+					        $this->setFrom($user->getEmailAddress(), $user->getName());
 
-				        break;
+					        break;
 
-			        default:
-				        $this->addAnAddress($kind, $user->getEmailAddress(), $user->getName());
+				        default:
+					        $this->addAnAddress($kind, $user->getEmailAddress(), $user->getName());
 
+			        }
 		        }
 	        }
-        }
 
-        $this->addReplyTo("dev@bryanselner.com", "JobScooper Deveopers" );
-	    $this->SMTPOptions = array(
-            'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true
-            )
-        );
+	        $this->addReplyTo("dev@bryanselner.com", "JobScooper Deveopers" );
+		    $this->SMTPOptions = array(
+	            'ssl' => array(
+	                'verify_peer' => false,
+	                'verify_peer_name' => false,
+	                'allow_self_signed' => true
+	            )
+	        );
 
-	    // Set word wrap to 120 characters
-	    $this->WordWrap = 120;
+		    // Set word wrap to 120 characters
+		    $this->WordWrap = 120;
 
-	    // Add attachments
-	    if(!empty($arrDetailsAttachFiles) && is_array($arrDetailsAttachFiles))
-        {
-            foreach($arrDetailsAttachFiles as $detailsAttach)
-            {
-                $this->addAttachment($detailsAttach);
-            }
-        }
+		    // Add attachments
+		    if(!empty($arrDetailsAttachFiles) && is_array($arrDetailsAttachFiles))
+	        {
+	            foreach($arrDetailsAttachFiles as $detailsAttach)
+	            {
+	                $this->addAttachment($detailsAttach);
+	            }
+	        }
 
-	    $this->isHTML(true);                                            // Set email format to HTML
+		    $this->isHTML(true);                                            // Set email format to HTML
 
-        if(strlen($strBodyText) == 0 || strlen($strBodyHTML) > 0)
-            $strBodyText = strip_tags($strBodyHTML);
+	        if(strlen($strBodyText) == 0 || strlen($strBodyHTML) > 0)
+	            $strBodyText = strip_tags($strBodyHTML);
 
-	    $this->Body    = $strBodyHTML;
-	    $this->AltBody = $strBodyText;
-	    $this->Subject = $subject;
+		    $this->Body    = $strBodyHTML;
+		    $this->AltBody = $strBodyText;
+		    $this->Subject = $subject;
 
 
-        $ret = $this->send();
-        if($ret !== true)
-        {
-            //
-            // If sending the email fails, try again but this time with SMTP debug
-            // enabled so we have any idea what the issue might be in the log.
-            // If we don't do this, we just get "failed" without any useful details.
-            //
-            $msg = "Failed to send notification email with error = ".$this->ErrorInfo . PHP_EOL . "Retrying email send with debug enabled to log error details...";
-            LogError($msg);
-	        $this->SMTPDebug = 1;
-            $ret = $this->send();
-            if($ret === true) return $ret;
+	        $ret = $this->send();
+	        if($ret !== true)
+	        {
+	            try
+		        {
+		            $alertsConfig = array();
+			        foreach ($alerts_users as $kind => $user)
+			            $alertsConfig[$kind] = $user->toArray();
 
-            $msg = "Failed second attempt to send notification email.  Debug error details should be logged above.  Error: " . PHP_EOL .$this->ErrorInfo;
-            LogError($msg);
-            throw new Exception($msg);
+		            $errorInfo = array(
+		                "phpmailer_error" => $this->ErrorInfo,
+				        "phpmail_settings" => objectToArray($this),
+				        "alerts_users" => $alertsConfig
+			        );
+		            $jsonErrorInfo = encodeJSON($errorInfo);
 
-        }
-        else
-        {
-            LogMessage("Email sent to " . getArrayValuesAsString($this->getAllRecipientAddresses()) . " from " . $this->From);
-        }
-        return $ret;
+		            //
+		            // If sending the email fails, try again but this time with SMTP debug
+		            // enabled so we have any idea what the issue might be in the log.
+		            // If we don't do this, we just get "failed" without any useful details.
+		            //
+		            $msg = "Failed to send notification email with error = {$this->ErrorInfo}.   ". PHP_EOL . PHP_EOL . "Full details = {$jsonErrorInfo} ". PHP_EOL . PHP_EOL . "Retrying email send with debug enabled to log error details...";
+		        } catch (\Exception $ex)
+		        {
+			        $msg = "Failed to send notification email with error = {$this->ErrorInfo}.   Retrying email send with debug enabled to log error details...";
+		        }
+	            LogError($msg);
+		        $this->SMTPDebug = 1;
+	            $ret = $this->send();
+	            if($ret === true) return $ret;
 
+	            $msg = "Failed second attempt to send notification email.  Debug error details should be logged above.  Error: " . PHP_EOL .$this->ErrorInfo;
+	            LogError($msg);
+	            throw new Exception($msg);
+
+	        }
+	        else
+	        {
+	            LogMessage("Email sent to " . getArrayValuesAsString($this->getAllRecipientAddresses()) . " from " . $this->From);
+	        }
+	        return $ret;
+	    }
+		catch (Exception $ex)
+		{
+			handleException($ex, "Failed to send notification email with error = %s", true);
+		}
     }
 
-    public function addMailCssToHTMLFile($strFilePath)
+	/**
+	 * @param $strFilePath
+	 * @throws \Exception
+	 */
+	public function addMailCssToHTMLFile($strFilePath)
     {
 
         $strHTMLContent = file_get_contents($strFilePath);
@@ -164,7 +210,14 @@ class JobsMailSender extends PHPMailer
 
     }
 
-    public function addMailCssToHTML($strHTML)
+	/**
+	 * @param $strHTML
+	 *
+	 * @return string
+	 * @throws \Exception
+	 * @throws \TijsVerkoyen\CssToInlineStyles\Exception
+	 */
+	public function addMailCssToHTML($strHTML)
     {
         $cssData = array(
             'color-primary' => "#49332D",
