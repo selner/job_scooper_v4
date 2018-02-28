b1b6bdb54e8cfbc9ffca1adc4736aeb990727f72
Bryan Selner <dev@recoilvelocity.com>
2018-02-28 12:47:23 -0800

    Committing model classes for previous schema change

--- src/JobScooper/DataAccess/Base/JobPosting.php
+++ src/JobScooper/DataAccess/Base/JobPosting.php
@@ -3063,31 +3063,6 @@ abstract class JobPosting implements ActiveRecordInterface
         return $this->getUserJobMatches($query, $con);
     }
 
-
-    /**
-     * If this collection has already been initialized with
-     * an identical criteria, it returns the collection.
-     * Otherwise if this JobPosting is new, it will return
-     * an empty collection; or if this JobPosting has previously
-     * been saved, it will retrieve related UserJobMatches from storage.
-     *
-     * This method is protected by default in order to keep the public
-     * api reasonable.  You can provide public methods for those you
-     * actually need in JobPosting.
-     *
-     * @param      Criteria $criteria optional Criteria object to narrow the query
-     * @param      ConnectionInterface $con optional connection object
-     * @param      string $joinBehavior optional join type to use (defaults to Criteria::LEFT_JOIN)
-     * @return ObjectCollection|ChildUserJobMatch[] List of ChildUserJobMatch objects
-     */
-    public function getUserJobMatchesJoinUserSearchSiteRunFromUJM(Criteria $criteria = null, ConnectionInterface $con = null, $joinBehavior = Criteria::LEFT_JOIN)
-    {
-        $query = ChildUserJobMatchQuery::create(null, $criteria);
-        $query->joinWith('UserSearchSiteRunFromUJM', $joinBehavior);
-
-        return $this->getUserJobMatches($query, $con);
-    }
-
     /**
      * Clears out the collUserFromUJMs collection
      *
