937691d3bd1e43e8728921f65e04fa877e3f848c
Bryan Selner <dev@recoilvelocity.comdev@recoilvelocity.comdev@recoilvelocity.comdev@recoilvelocity.com>
2018-05-18 22:29:51 -0700

    More work on deploying via docker compose & build caching

--- scripts/deploy_docker_compose.sh
+++ scripts/deploy_docker_compose.sh
@@ -18,13 +18,24 @@ fi
 
 cd ..
 
+##
+# Remove the old values from any .env file
+##
+sed '/JS_DEPLOY_DATE/ d' .env > envtmp1
+sed '/GIT_COMMIT_HASH/ d' envtmp1 > envtmp2
+sed '/BRANCH/ d'  envtmp2 > .env
+rm envtmp*
+
+# GIT_COMMIT_HASH_RAW=$(git rev-parse --short HEAD)
+#BRANCH=`git symbolic-ref --short HEAD`
+
 echo "JS_DEPLOY_DATE=$(date +%s)" >> .env
-echo "GIT_COMMIT_HASH=$(git rev-parse --short HEAD)" >> .env
+echo "GIT_COMMIT_HASH=$(echo $(git rev-parse --short HEAD) | tr '[A-Z]' '[a-z]')" >> .env
+echo "BRANCH=$(echo $(git symbolic-ref --short HEAD))" >> .env
+echo "BRANCH_LC=$(echo $(git symbolic-ref --short HEAD) | tr '[A-Z]' '[a-z]')" >> .env
 
 echo "Docker .env variables are set to: "
 cat .env
-BRANCH=`git symbolic-ref --short HEAD`
-echo "BRANCH = $BRANCH"
 
 
 echo "***************************************************************"
@@ -41,9 +52,12 @@ echo ""
 echo "***************************************************************"
 docker-compose down --remove-orphans
 
+docker-compose build
+
 docker-compose pull
 
 docker-compose config
+
 
 docker-compose up -d
 
