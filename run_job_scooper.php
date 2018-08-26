cbd49e7bac794f42c5e164f44154004e9521a0cd
Bryan Selner <dev@recoilvelocity.com>
2018-08-26 12:14:25 -0700

    single quotes for doubles

--- run_job_scooper.php
+++ run_job_scooper.php
@@ -19,13 +19,13 @@
 ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 $lineEnding = ini_get('auto_detect_line_endings');
 ini_set('auto_detect_line_endings', true);
-date_default_timezone_set("America/Los_Angeles");
+date_default_timezone_set('America/Los_Angeles');
 gc_enable();
 
-const C__APPNAME__ = "jobs_scooper";
-const __APP_VERSION__ = "v4.1.0-use-propel-orm";
-const C__SRC_LOCATION = "https://github.com/selner/job_scooper_v4";
-const C__STR_USER_AGENT__ = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
+const C__APPNAME__ = 'jobs_scooper';
+const __APP_VERSION__ = 'v4.1.0-use-propel-orm';
+const C__SRC_LOCATION = 'https://github.com/selner/job_scooper_v4';
+const C__STR_USER_AGENT__ = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36';
 
 define('MAX_FILE_SIZE', 5000000);
 define('__ROOT__', __DIR__);
@@ -34,7 +34,7 @@ $autoload = implode(DIRECTORY_SEPARATOR, array(__ROOT__, 'vendor', 'autoload.php
 if (file_exists($autoload)) {
     require_once($autoload);
 } else {
-    trigger_error("Composer required to run this app.");
+    trigger_error('Composer required to run this app.');
 }
 
 $cmdline = new \JobScooper\Utils\DocOptions(__FILE__);
@@ -43,10 +43,10 @@ $arguments = $cmdline->getAll();
 \JobScooper\Utils\Settings::setValue('command_line_args', $arguments);
 
 try {
-    $config = new \JobScooper\Utils\ConfigInitializer($arguments["config"]);
+    $config = new \JobScooper\Utils\ConfigInitializer($arguments['config']);
     $config->initialize();
 } catch (Exception $e) {
-    throw new \Exception("Could not load {$arguments["config"]}: " . $e->getMessage());
+    throw new \Exception("Could not load {$arguments['config']}: " . $e->getMessage());
 }
 
 $classRunJobs = new \JobScooper\Manager\StageManager($config);
