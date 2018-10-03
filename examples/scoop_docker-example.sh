083531823604c9a8489cf3fc16333520609c7645
Bryan Selner <dev@recoilvelocity.com>
2018-10-03 16:19:37 -0700

    fixed typo

--- examples/scoop_docker-example.sh
+++ examples/scoop_docker-example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /opt/jobs_scooper
 
-php run_job_scooperrun_job_scooper.php /var/local/jobs_scooper/configs/evan/job_scooper_config.ini --jobsite all
+php run_job_scooper.php /var/local/jobs_scooper/configs/evan/job_scooper_config.ini --jobsite all
 
 php run_job_scooper.php /var/local/jobs_scooper/configs/bryan/job_scooper_config.ini --jobsite indeed --debug
 
