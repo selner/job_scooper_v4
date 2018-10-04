3190db0559a1d083f4e6e58db52b7af52a81db7a
Bryan Selner <dev@recoilvelocity.com>
2018-10-04 09:33:41 -0700

    removed unused import

--- python/pyJobNormalizer/lib/tasks/dedupe_jobs.py
+++ python/pyJobNormalizer/lib/tasks/dedupe_jobs.py
@@ -17,8 +17,6 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 ###########################################################################
-import pymysql
-
 import pandas
 from datetime import *
 from ..helpers import write_json, load_ucsv, load_json
